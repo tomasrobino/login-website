cecc4817af41f8ac8a9de75778ad53a68c7cb238
tomasrobino <tomaserobino@gmail.com>
2024-03-25 23:14:25 +0100

    server authenticate username & password done

--- server/express.js
+++ server/express.js
@@ -3,13 +3,25 @@ const app = express();
 const cors = require("cors");
 const body = require("body-parser")
 const port = 5000;
-
+const bcrypt = require("bcrypt");
 
 app.use(cors(), body.json());
 
+//Global variable for amount of salt in bcrypt
+const SALTROUNDS = 10;
+
+//Provisional "database" TODO: real database
+const users = new Map();
+users.set("username", "password");
+users.set("exampleUser", "examplePassword");
+
 app.post("/authenticate", (req, res) => {
-    res.send(true);
-    //res.end(req)
+    if (req.body?.username && req.body?.password) {
+        if (users.get(req.body.username) === req.body.password) {
+            const passwordHash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(SALTROUNDS));
+            res.send(passwordHash);
+        } else res.send("notfound");     
+    }
 })
 
 app.listen(port, () => {
