3b3470e27121b39eeb4baf4c8cca253843e12205
tomasrobino <tomaserobino@gmail.com>
2024-03-26 11:50:38 +0100

    deleted unused imports & token state

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
-import Login from './Login';
 import './App.css';
-import { useState } from 'react';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function App() {
-    const [token, setToken] = useState();
-
     return (
         <div className="App">
             <Link to="/auth">
